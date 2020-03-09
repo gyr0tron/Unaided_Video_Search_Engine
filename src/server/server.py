5ac8c24f795713876394f4476183176ab7b53789
gyrotron <simmarkalsi@gmail.com>
2020-03-09 17:55:06 +0530

    now testing cron jobs

--- src/server/server.py
+++ src/server/server.py
@@ -1,14 +1,30 @@
 import datetime
 from werkzeug.utils import secure_filename
 import os
+import flask
 from flask import Flask, render_template, request, make_response, jsonify, Response
 from video_splitter import split_generate
 import subprocess
 from math import sqrt
 from time import sleep
+from shelljob import proc
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = '../../data/raw'
+
+input_video_paths = list()
+input_video_names = list()
+input_vid_tmp = "../../data/raw/input.mp4"
+PROCESSED_PATH = '../../data/processed/'
+STATS_FILE_PATH = f'{PROCESSED_PATH}stats.csv'
+RAW_PATH = '../../data/raw/'
+with os.scandir(RAW_PATH) as entries:
+    for entry in entries:
+        input_video_names.append(entry.name)
+        input_video_paths.append(f'../../data/raw/{entry.name}')
+
+processed_images_path = f'{PROCESSED_PATH}images'
+processed_scenes_path = f'{PROCESSED_PATH}scenes'
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -57,20 +73,45 @@ def index():
     return render_template('public/process.html')
 
 
-@app.route('/stream_sqrt')
+@app.route('/stream')
 def stream():
-    def generate():
-        proc = subprocess.Popen(
-            split_generate(),  # call something with a lot of output so we can see it
-            shell=True,
-            stdout=subprocess.PIPE
-        )
+    process_cli = f'scenedetect --input {input_video_paths[0]} --output {PROCESSED_PATH} --stats {STATS_FILE_PATH} detect-content --min-scene-len 30 --threshold 27 list-scenes save-images --output {processed_images_path} --num-images 1 split-video --output {processed_scenes_path}'
+    g = proc.Group()
+    p = g.run(process_cli)
 
-        # for i in range(500):
-        for c in iter(proc.stdout.readlines()):
-            yield '{}\n'.format(c.decode('utf-8').rstrip('\n'))
-        # sleep(1)
-    return app.response_class(generate(), mimetype='text/plain')
+    def read_process():
+        while g.is_pending():
+            lines = g.readlines()
+            for proc, line in lines:
+                yield line
+
+    return Response(read_process(), mimetype='text/event-stream')
+
+
+@app.route("/search-page")
+def search_page_return():
+    return render_template("public/search-page.html")
+
+
+# @app.route('/process')
+# def get_page():
+#     return render_template('public/new.html')
+# @app.route('/stream_sqrt')
+# def stream():
+#     def generate():
+#         process_cli = f'scenedetect --input {input_video_paths[0]} --output {PROCESSED_PATH} --stats {STATS_FILE_PATH} detect-content --min-scene-len 30 --threshold 27 list-scenes save-images --output {processed_images_path} --num-images 1 split-video --output {processed_scenes_path}'
+#         proc = subprocess.run(
+#             # call something with a lot of output so we can see it
+#             process_cli,
+#             shell=True,
+#             stdout=subprocess.PIPE
+#         )
+
+#         # for i in range(500):
+#         for c in iter(proc.stdout.readlines()):
+#             yield '{}\n'.format(c.decode('utf-8').rstrip('\n'))
+#         # sleep(1)
+#     return app.response_class(generate(), mimetype='text/plain')
 
 
 if __name__ == "__main__":
