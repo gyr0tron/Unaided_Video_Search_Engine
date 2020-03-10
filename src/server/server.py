0c9bd4aafa30d61ea6f49bdee3e86eb729018bf0
gyrotron <simmarkalsi@gmail.com>
2020-03-10 23:53:00 +0530

    added docker for flask

--- src/server/server.py
+++ src/server/server.py
@@ -3,87 +3,63 @@ from werkzeug.utils import secure_filename
 import os
 import flask
 from flask import Flask, render_template, request, make_response, jsonify, Response
-from video_splitter import split_generate
 import subprocess
-from math import sqrt
 from time import sleep
 from shelljob import proc
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = '../../data/raw'
+# app.config['UPLOAD_FOLDER'] = '../../data/raw'
 
-input_video_paths = list()
-input_video_names = list()
-input_vid_tmp = "../../data/raw/input.mp4"
-PROCESSED_PATH = '../../data/processed/'
-STATS_FILE_PATH = f'{PROCESSED_PATH}stats.csv'
-RAW_PATH = '../../data/raw/'
-with os.scandir(RAW_PATH) as entries:
-    for entry in entries:
-        input_video_names.append(entry.name)
-        input_video_paths.append(f'../../data/raw/{entry.name}')
+# input_video_paths = list()
+# input_video_names = list()
+# input_vid_tmp = "../../data/raw/input.mp4"
+# PROCESSED_PATH = '../../data/processed/'
+# STATS_FILE_PATH = f'{PROCESSED_PATH}stats.csv'
+# RAW_PATH = '../../data/raw/'
+# with os.scandir(RAW_PATH) as entries:
+#     for entry in entries:
+#         input_video_names.append(entry.name)
+#         input_video_paths.append(f'../../data/raw/{entry.name}')
 
-processed_images_path = f'{PROCESSED_PATH}images'
-processed_scenes_path = f'{PROCESSED_PATH}scenes'
+# processed_images_path = f'{PROCESSED_PATH}images'
+# processed_scenes_path = f'{PROCESSED_PATH}scenes'
 
 
 @app.route("/", methods=["GET", "POST"])
 def upload_video():
 
-    if request.method == "POST":
+    # if request.method == "POST":
 
-        file = request.files["file"]
-        temp_file = file.filename.split(".")
-        file.save(os.path.join(
-            app.config['UPLOAD_FOLDER'], secure_filename(temp_file[0] + str(datetime.datetime.now().isoformat()) + "." + temp_file[1])))
-        print("File uploaded")
-        print(file)
+    #     file = request.files["file"]
+    #     temp_file = file.filename.split(".")
+    #     file.save(os.path.join(
+    #         app.config['UPLOAD_FOLDER'], secure_filename(temp_file[0] + str(datetime.datetime.now().isoformat()) + "." + temp_file[1])))
+    #     print("File uploaded")
+    #     print(file)
 
-        res = make_response(jsonify({"message": "File uploaded"}), 200)
+    #     res = make_response(jsonify({"message": "File uploaded"}), 200)
 
-        return res
+    #     return res
 
     return render_template("public/upload_video.html")
 
 
 @app.route('/yield')
-# #show started
-# #fuction call
-# #function returns done
-# #show done
-# def index():
-#     def inner():
-#         with open('test.log', 'w') as f:
-#             proc = subprocess.Popen(
-#                 split_generate(),  # call something with a lot of output so we can see it
-#                 shell=True,
-#                 stdout=subprocess.PIPE
-#             )
-#             # for line in iter(lambda: proc.stdout.read(1), ''):
-#             #     # Don't need this just shows the text streaming
-#             #     # time.sleep(1)
-#             #     yield line + '<br/>\n'
-#             for c in iter(lambda: proc.stdout.read(1), ''):  # replace '' with b'' for Python 3
-#                 sys.stdout.write(c)
-#                 f.write(c)
-#     # text/html is required for most browsers to show th$
-#     return Response(inner(), mimetype='text/html')
 def index():
-    # render the template (below) that will use JavaScript to read the stream
     return render_template('public/process.html')
 
 
 @app.route('/stream')
 def stream():
-    process_cli = f'scenedetect --input {input_video_paths[0]} --output {PROCESSED_PATH} --stats {STATS_FILE_PATH} detect-content --min-scene-len 30 --threshold 27 list-scenes save-images --output {processed_images_path} --num-images 1 split-video --output {processed_scenes_path}'
-    g = proc.Group()
-    p = g.run(process_cli)
+    # process_cli = f'scenedetect --input {input_video_paths[0]} --output {PROCESSED_PATH} --stats {STATS_FILE_PATH} detect-content --min-scene-len 30 --threshold 27 list-scenes save-images --output {processed_images_path} --num-images 1 split-video --output {processed_scenes_path}'
+    # g = proc.Group()
+    # p = g.run(process_cli)
 
-    def read_process():
-        while g.is_pending():
-            lines = g.readlines()
-            for proc, line in lines:
-                yield line
+    # def read_process():
+    #     while g.is_pending():
+    #         lines = g.readlines()
+    #         for proc, line in lines:
+    #             yield line
 
     return Response(read_process(), mimetype='text/event-stream')
 
@@ -93,27 +69,6 @@ def search_page_return():
     return render_template("public/search-page.html")
 
 
-# @app.route('/process')
-# def get_page():
-#     return render_template('public/new.html')
-# @app.route('/stream_sqrt')
-# def stream():
-#     def generate():
-#         process_cli = f'scenedetect --input {input_video_paths[0]} --output {PROCESSED_PATH} --stats {STATS_FILE_PATH} detect-content --min-scene-len 30 --threshold 27 list-scenes save-images --output {processed_images_path} --num-images 1 split-video --output {processed_scenes_path}'
-#         proc = subprocess.run(
-#             # call something with a lot of output so we can see it
-#             process_cli,
-#             shell=True,
-#             stdout=subprocess.PIPE
-#         )
-
-#         # for i in range(500):
-#         for c in iter(proc.stdout.readlines()):
-#             yield '{}\n'.format(c.decode('utf-8').rstrip('\n'))
-#         # sleep(1)
-#     return app.response_class(generate(), mimetype='text/plain')
-
-
 if __name__ == "__main__":
     # import sys
-    app.run(threaded=False, debug=False)
+    app.run(host="0.0.0.0", debug=True, threaded=False)
--- src/video_splitter.py
+++ src/video_splitter.py
@@ -1,45 +1,46 @@
 from __future__ import print_function
 import os
 
-import pandas as pd
 import scenedetect
 from scenedetect.video_manager import VideoManager
 from scenedetect.scene_manager import SceneManager
 from scenedetect.frame_timecode import FrameTimecode
 from scenedetect.stats_manager import StatsManager
 from scenedetect.detectors import ContentDetector
-from scenedetect.detectors import ThresholdDetector
+from scenedetect.video_splitter import split_video_mkvmerge
+from scenedetect.video_splitter import split_video_ffmpeg
+
+import pandas as pd
 
 input_video_paths = list()
 input_video_names = list()
-PROCESSED_PATH = '../../data/processed/'
+input_vid_tmp = "../data/raw/input.mp4"
+PROCESSED_PATH = '../data/processed/'
 STATS_FILE_PATH = f'{PROCESSED_PATH}stats.csv'
-RAW_PATH = '../../data/raw/'
+RAW_PATH = '../data/raw/'
 with os.scandir(RAW_PATH) as entries:
     for entry in entries:
         input_video_names.append(entry.name)
-        input_video_paths.append(f'../../data/raw/{entry.name}')
+        input_video_paths.append(f'../data/raw/{entry.name}')
 
 
-def main():
-
+def split_generate():
+    print("test1")
+    print("test1")
+    print("test1")
+    print("test1")
     video_manager = VideoManager(input_video_paths)
     stats_manager = StatsManager()
     scene_manager = SceneManager(stats_manager)
-    # scene_manager.add_detector(ContentDetector(threshold=22, min_scene_len=90))
-    scene_manager.add_detector(ThresholdDetector(
-        threshold=50, min_percent=0.50, min_scene_len=30, fade_bias=0.0, add_final_scene=False, block_size=8))
+    scene_manager.add_detector(ContentDetector(min_scene_len=30))
     base_timecode = video_manager.get_base_timecode()
 
     try:
+        # If stats file exists, load it.
         if os.path.exists(STATS_FILE_PATH):
+            # Read stats from CSV file opened in read mode:
             with open(STATS_FILE_PATH, 'r') as stats_file:
                 stats_manager.load_from_csv(stats_file, base_timecode)
-
-        # start_time = base_timecode + 20     # 00:00:00.667
-        # end_time = base_timecode + 20.0     # 00:00:20.000
-        # # Set video_manager duration to read frames from 00:00:00 to 00:00:20.
-        # video_manager.set_duration(start_time=start_time, end_time=end_time)
 
         video_manager.set_downscale_factor()
 
@@ -47,8 +48,8 @@ def main():
 
         scene_manager.detect_scenes(frame_source=video_manager)
 
+        # Obtain list of detected scenes.
         scene_list = scene_manager.get_scene_list(base_timecode)
-
         processing_data = []
         print('List of scenes obtained:')
         for i, scene in enumerate(scene_list):
@@ -64,27 +65,25 @@ def main():
         # print(processing_data_df)
         processing_data_df.to_csv(f'{PROCESSED_PATH}/timecodes.csv')
 
+        # We only write to the stats file if a save is required:
         if stats_manager.is_save_required():
             with open(STATS_FILE_PATH, 'w') as stats_file:
                 stats_manager.save_to_csv(stats_file, base_timecode)
 
-        # if (scenedetect.video_splitter.is_ffmpeg_available()):
-        #     print('ffmpeg is available. Now loading ffmpeg!')
-            # for vid_file in input_video_names:
-            #     scenedetect.video_splitter.split_video_ffmpeg(
-            #         input_video_paths, scene_list, vid_file, vid_file,
-            #         arg_override='-c:v libx264 -preset fast -crf 21 -c:a copy', hide_progress=False, suppress_output=False)
-        if (scenedetect.video_splitter.is_mkvmerge_available()):
-            print('mkvmerge  is available. Now loading mkvmerge!')
-            for vid_file in input_video_names:
-                scenedetect.video_splitter.split_video_mkvmerge(
-                    input_video_paths, scene_list, 'new/'+vid_file, vid_file, suppress_output=False)
-        else:
-            print('Install ffmpeg or mkvmerge for scene split')
+        # split_video_mkvmerge(
+        #     input_video_paths, scene_list, output_file_prefix=f'{input_video_names[0]}', video_name='ayy.mp4', suppress_output=False)
+
+        # split_video_ffmpeg(
+        #     input_video_paths=input_video_paths[0], scene_list=scene_list, output_file_template=f'{input_video_names[0]}', video_name='vid', hide_progress=False, suppress_output=False)
+        processed_images_path = f'{PROCESSED_PATH}images'
+        processed_scenes_path = f'{PROCESSED_PATH}scenes'
+
+        # os.system(
+        #     f'scenedetect --input {input_video_paths[0]} --output {PROCESSED_PATH} --stats {STATS_FILE_PATH} detect-content --min-scene-len 30 --threshold 27 list-scenes save-images --output {processed_images_path} --num-images 1 split-video --output {processed_scenes_path}')
 
     finally:
         video_manager.release()
 
 
 if __name__ == "__main__":
-    main()
+    split_generate()
